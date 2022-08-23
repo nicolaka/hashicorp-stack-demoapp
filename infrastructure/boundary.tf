2715fd98d88798d5b20c4d7095b73e7d0f6b225a
nicolaka <nicolaka@gmail.com>
2022-08-23 20:25:13 +0000

    new module v 0.0.4

--- infrastructure/boundary.tf
+++ infrastructure/boundary.tf
@@ -9,7 +9,7 @@ resource "random_password" "boundary_database" {
 module "boundary" {
   depends_on                   = [module.vpc]
   source                       = "app.terraform.io/nico-tfc/boundary/aws"
-  version                      = "0.0.3"
+  version                      = "v0.0.4"
   vpc_id                       = module.vpc.vpc_id
   vpc_cidr_block               = module.vpc.vpc_cidr_block
   public_subnet_ids            = module.vpc.public_subnets
